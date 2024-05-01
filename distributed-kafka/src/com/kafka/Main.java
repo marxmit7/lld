e64b54a7a0c681a2bac94227ede5a94c9a226391
amitkumar.ry <amitkumar.ry@flipkart.com>
2024-05-01 18:52:26 +0530

    update

--- distributed-kafka/src/com/kafka/Main.java
+++ distributed-kafka/src/com/kafka/Main.java
@@ -15,17 +15,16 @@ public class Main{
         distributedQueue.createTopic("topic_b");
         distributedQueue.createTopic("topic_c");
 
-        Message message_a = new Message("ma");
-        Message message_b = new Message("mb");
-        Message message_c = new Message("mc");
+        Message message_a = new Message("message_a");
+        Message message_b = new Message("message_b");
 
         Producer producer = new Producer(distributedQueue);
         producer.publish(message_a, "topic_a");
-        producer.publish(message_b, "topic_c");
-        producer.publish(message_c, "topic_b");
+        producer.publish(message_b, "topic_b");
 
         Consumer consumer = new Consumer(distributedQueue,"c1");
 
         consumer.subscribe("topic_a");
+        consumer.subscribe("topic_b");
     }
 }
